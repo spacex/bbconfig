ba0c0d3fa3af9b928225588abcd33558c71d0709
Michael Williams <mspacex@gmail.com>
2012-01-04 16:35:38 -0600

    Project view url now allows name or id lookup.

--- urls.py
+++ urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'^$', 'bbconfig.views.index', name='index'),
-    url(r'^project/(\d+)/$', 'bbconfig.views.project', name='project'),
+    url(r'^project/(\d+)/$', 'bbconfig.views.project_byid', name='project'),
+    url(r'^project/(.*)/$', 'bbconfig.views.project_byname', name='project'),
 )
--- views.py
+++ views.py
@@ -5,9 +5,17 @@ from bbconfig.models import *
 def index(request):
 	return render(request, 'index.html')
 
-def project(request, project_id):
+def project_byid(request, project_id):
 	project = get_object_or_404(Project.objects.filter(id=project_id))
 
+	return project_render(request, project)
+
+def project_byname(request, name):
+	project = get_object_or_404(Project.objects.filter(name=name))
+
+	return project_render(request, project)
+
+def project_render(request, project):
 	ordered_commands = project.command_set.order_by('category', 'sequence')
 
 	dic = {}
